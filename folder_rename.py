4d3eb91978a955688b01edab3d567cda90bf8442
nickk2002 <nicolae.filat@yahoo.com>
2020-12-22 20:44:14 +0200

    Fixed y/Y Capitalization in folder_rename.py

--- folder_rename.py
+++ folder_rename.py
@@ -1,6 +1,7 @@
 import os
 
 import regex as re
+import crayons
 
 
 def is_int(s):
@@ -63,9 +64,11 @@ def run_folder_rename_dir(directory):
         renamed_folder_name = modify_name_regex(nume_folder)
         print(nume_folder, "=>", renamed_folder_name)
     answer = input("Do you really want to modify the names? y/n ")
-    if answer == 'Y':
+    if answer == 'y' or answer == 'Y':
+        print(crayons.green("Ok! Now we will modify!"))
         for (nume_folder, path) in directory_files:
             renamed_folder_name = modify_name_regex(nume_folder)
             if nume_folder != renamed_folder_name:
                 path_modificat = path.replace(nume_folder, renamed_folder_name)
+                print(crayons.normal(f'{nume_folder} -> {renamed_folder_name}'))
                 os.rename(path, path_modificat)
