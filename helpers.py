5819bf877a05140d5493a59158e556769d97aad0
nickk2002 <nicolae.filat@yahoo.com>
2020-12-23 21:33:49 +0200

    added a wait_to_load function, helper for selenium scraping

--- helpers.py
+++ helpers.py
@@ -1,5 +1,12 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as Wait
+
+
+def wait_to_load(browser, search_type, input, wait_time = 1):
+    return Wait(browser, wait_time).until(
+        EC.visibility_of_element_located((search_type, input)))
 
 def get_html_from_url(url):
     request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
