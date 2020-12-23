09f866f8ca2c93247fabb774108b358711214f05
nickk2002 <nicolae.filat@yahoo.com>
2020-12-23 21:32:33 +0200

    Created a movie class that stores information from imdb

---
+++ movie.py
@@ -0,0 +1,27 @@
+import os
+import regex as re
+from Screapers.imdb_screaper import IMDBScreaper
+
+
+class Movie:
+    def __init__(self, movie_path, mkv_name=""):
+        self.folder_path = movie_path
+        self.mkv_name = mkv_name
+        self.folder_name = os.path.basename(movie_path)
+        self.name = self.get_movie_name_without_year()
+
+        self.get_information_from_imdb()
+
+    def get_information_from_imdb(self):
+        information = IMDBScreaper().run_screaper(self.name)
+        self.imdb_id = information["imdb_id"]
+        self.information = information
+
+    def get_movie_name_without_year(self):
+        return re.search("[a-zA-Z ]+", self.folder_name).group()
+
+    def __str__(self):
+        return self.name
+
+# movie = Movie("E:\Quick access\Documents\Info\Proiecte mari\Python\Movie Project\Folder Test\Once upon a time...(2002)","")
+# movie.get_movie_name_without_year()
