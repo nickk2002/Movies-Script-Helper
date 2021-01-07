65a027a0da907e24edd2a6748e5db5f1f5d9c60d
nickk2002 <nicolae.filat@yahoo.com>
2021-01-07 08:48:26 +0200

    Created app folder and created packages. Added the -tor option to run shell download from filelist

---
+++ app/Screapers/settings.py
@@ -0,0 +1,10 @@
+class FileListSettings:
+    user = "user",
+    password = "pass"
+
+
+class Selenium:
+    CHROME_DRIVER_PATH = "E:\Quick access\Documents\Info\Plugins\ChromeDriver\chromedriver.exe"
+
+class Download:
+    downlaod_folder = "E:\Quick access\Downloads"
