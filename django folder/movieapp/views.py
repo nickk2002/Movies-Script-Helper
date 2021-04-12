a55fc1d6bb3626427f2d3e474911f27aeb9d4542
nickk2002 <nicolae.filat@yahoo.com>
2021-04-12 11:55:54 +0300

    Removed unused folders

--- django folder/movieapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,render_to_response
-from django.http import HttpRequest,HttpResponse
-
-def index(request):
-    print("I Am here")
-    context = {
-        'name': "Thor "
-    }
-    return render(request,'movieapp/design/design.html',context)
